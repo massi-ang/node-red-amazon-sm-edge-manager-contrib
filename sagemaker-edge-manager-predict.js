ac6349be7864b92f66a2369069b008f719a8b5c3
Massimiliano Angelino <angmas@amazon.com>
2021-08-24 15:57:49 +0200

    error handling and new node logic

--- sagemaker-edge-manager-predict.js
+++ sagemaker-edge-manager-predict.js
@@ -21,71 +21,120 @@ module.exports = function(RED) {
         req.setName(node.model.modelName);
         req.setUrl(node.model.modelUri);
 
-        client.service.loadModel(req, function(err, response) {
-            if (err) {
-                if (err.toString().includes('6')) {
-                    node.warn(`Model ${node.model.modelName} is already loaded. It might not be the model you want.`);
-                } else {
-                    node.status({fill:"red",shape:"ring",text:"error loading model"});
-                    node.error(`Model URL: ${node.model.modelUri} Error ${err}`);
-                    return;
-                }
-            } 
-            var modelReq = new messages.DescribeModelRequest();
-        
-            modelReq.setName(node.model.modelName);
-
-            client.service.describeModel(modelReq, function (err, response) {
-                if (err) {
-                    node.status({fill:"red",shape:"ring",text:"cannot retrieve model"});
-                    node.error(err);
-                    return;
-                }
-                const model = response.getModel();
-                // We assume only one tensor model for now
-                const metadata = model.getInputTensorMetadatasList();
-                if (metadata.length > 1) {
-                    node.status({fill:"red",shape:"ring",text:"model requires >1 tensors"});
-                    return;
-                }
-                node.tensorMetadata = metadata[0];
-                node.status({fill:"green",shape:"dot",text:"ready"});
-            });
-        })
-        
-
-		node.on("input", function(msg) {
-
-            var req = new messages.PredictRequest();
-            var t = new messages.Tensor();
-            var meta = new messages.TensorMetadata();
-
-            meta.setName(node.tensorMetadata.getName());
-            meta.setDataType(node.tensorMetadata.getDataType());
-            meta.setShapeList(node.tensorMetadata.getShapeList());
-            t.setTensorMetadata(meta);
-            t.setByteData(msg.payload);
-            req.setName(node.model.modelName);
-            req.setTensorsList([t]); 
-
-            client.service.predict(req, function(err, response) {
-                if (err) {
-				    node.status({fill:"red",shape:"ring",text:"error"});
-                    node.error("failed: " + err.toString(), msg);
-                    node.send([null, { err: err }]);
-    				return;
-				} 
-                const tensors = response.getTensorsList()
-                for (var i in tensors) {
-                    msg.payload = tensors[i].getByteData();
-                    msg.metadata = tensors[i].getTensorMetadata().getShapeList();
-                    msg.modelName = node.model.modelName;
-                    node.send([msg,null]);
-                }
-                node.status({fill:"green",shape:"ring",text:"ready"});
-				
-            })
+        const getModelDefinition = function() {
+            return new Promise((res, rej) => {
+                var modelReq = new messages.DescribeModelRequest();
             
+                modelReq.setName(node.model.modelName);
+
+                client.service.describeModel(modelReq, function (err, response) {
+                    if (err) {
+                        node.status({fill:"red",shape:"ring",text:"cannot retrieve model"});
+                        rej(err);
+                    }
+                    const model = response.getModel();
+                    // We assume only one tensor model for now
+                    const metadata = model.getInputTensorMetadatasList();
+                    if (metadata.length > 1) {
+                        node.status({fill:"red",shape:"ring",text:"model requires >1 tensors"});
+                        rej(new Error("Too many tensors"));
+                    }
+                    node.tensorMetadata = metadata[0];
+                    node.status({fill:"green",shape:"dot",text:"ready"});
+                    res();
+                });
+            });
+
+        }
+
+        const loadModel = function () {
+            return new Promise((res, rej) => {
+                client.service.loadModel(req, function(err, response) {
+                    if (err) {
+                        if (err.toString().includes('6')) {
+                            node.warn(`Model ${node.model.modelName} is already loaded. It might not be the model you want.`);
+                        } else {
+                            node.status({fill:"red",shape:"ring",text:"error loading model"});
+                            rej(new Error(`Model URL: ${node.model.modelUri} Error ${err}`));
+                            return;
+                        }
+                    } 
+                    node.modelName = node.model.modelName;
+                    getModelDefinition().then(res).catch(err => {node.error(err);});
+                })
+            })
+        };
+
+        const unloadModel = function() {
+            return new Promise((res, rej) => {
+                if (!node.modelName) {
+                    rej(new Error("no model loaded"));
+                }
+                var req = new messages.UnLoadModelRequest();
+                req.setName(node.modelName);
+
+                client.unLoadModel(req, function(err, response) {
+                    if (err) {
+                        node.status({fill:"red",shape:"ring",text:"error"});
+                        rej(new Error(err));
+                        return;
+                    } 
+                    node.status({});
+                    res(node.ModelName);
+                })
+            });
+        }
+        
+        loadModel().catch(err => {node.error(err)});
+
+		node.on("input", function(msg, send, done) {
+            const doPrediction = function() {
+                var req = new messages.PredictRequest();
+                var t = new messages.Tensor();
+                var meta = new messages.TensorMetadata();
+
+                meta.setName(node.tensorMetadata.getName());
+                meta.setDataType(node.tensorMetadata.getDataType());
+                meta.setShapeList(node.tensorMetadata.getShapeList());
+                t.setTensorMetadata(meta);
+                t.setByteData(msg.payload);
+                req.setName(node.model.modelName);
+                req.setTensorsList([t]); 
+
+                client.service.predict(req, function(err, response) {
+                    if (err) {
+                        node.status({fill:"red",shape:"ring",text:"error"});
+                        done(new Error("failed: " + err.toString()));
+                        return;
+                    } 
+                    const tensors = response.getTensorsList()
+                    for (var i in tensors) {
+                        msg.payload = tensors[i].getByteData();
+                        msg.metadata = tensors[i].getTensorMetadata().getShapeList();
+                        msg.modelName = node.model.modelName;
+                        send(msg);
+                    }
+                    done();
+                })
+            }
+            
+            if (msg.loadModel) {
+                loadModel().then(done).catch(err=>{done(err);});
+            } else if (msg.unLoadMode) {
+                unloadModel().then(m => { msg.modelName = m; send(msg);}).catch(err => {done(err);});
+            } else if (msg.refreshModel) {
+                if (!node.tensorMetadata) {
+                    done(new Error(`No model loaded`));
+                    return;
+                }
+                getModelDefinition().then(done).catch(err=>{done(err);});
+            } else {
+                if (!node.tensorMetadata) {
+                    done(new Error(`No model loaded`));
+                    return;
+                }
+                doPrediction();
+            }
 		});
 	}
 	RED.nodes.registerType("predict", SMEdgeManagerPredictNode);
